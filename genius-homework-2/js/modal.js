4705dae11c10bd6e70c9b18355ce7177ea6b62d5
Stepan <bodnar2005s@gmail.com>
2024-04-07 10:38:45 +0300

    homework-2

---
+++ genius-homework-2/js/modal.js
@@ -0,0 +1,8 @@
+const modal = document.querySelector(".backdrop");
+const modalBtnOpen = document.querySelector(".model-btn-open");
+const modalBtnClose = document.querySelector(".model-btn-close");
+
+const toggleModal = () => modal.classList.toggle("is-hidden");
+
+modalBtnOpen.addEventListener("click", toggleModal);
+modalBtnClose.addEventListener("click", toggleModal);
